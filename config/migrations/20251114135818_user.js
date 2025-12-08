a151eb6c645f6480f59ce44e03bc1fb7be804459
Tayeb Boulekzazel <ttayeb769@gmail.com>
2025-12-08 19:01:29 +0100

    UPD migration user

--- config/migrations/20251114135818_user.js
+++ config/migrations/20251114135818_user.js
@@ -4,22 +4,22 @@
  */
 exports.up = function (knex) {
   return knex.schema.createTable("user", (table) => {
-    table.increments("id").primary();;
+    table.increments("id").primary();
     table.string("name").notNullable();
     table.string("email").notNullable().unique();
     table.string("phone").notNullable().unique();
     table.string("password").notNullable();
-
-    table.enum("role", ["Admin", "Client", "RestaurantOwner", "Driver"])
+    table.string("picture").nullable();
+    table.boolean("IsActive").defaultTo(true);
+    table.boolean("isBanned").defaultTo(false);
+    table.boolean("verified").defaultTo(false);
+    table.timestamp("createdAt").defaultTo(knex.fn.now());
+    table.timestamp("updatedAt").defaultTo(knex.fn.now());
+    table.timestamp("lastLogin").nullable();
+    table
+      .enum("role", ["Admin", "Client", "RestaurantOwner", "Driver"])
       .notNullable()
       .defaultTo("Client");
-
-    table.string("picture").nullable();
-
-    table.boolean("verified").defaultTo(false);
-
-    table.timestamp("createdAt").defaultTo(knex.fn.now());
-    table.timestamp("lastLogin").nullable();
   });
 };
 
